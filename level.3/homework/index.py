683eae1d377f1dfdd45433f9c340dcdd737e5283
lashaabeslamidze <lashaabeslamidze10@gmail.com>
2024-06-17 08:06:20 +0300

    Create index.py
    commit

---
+++ level.3/homework/index.py
@@ -0,0 +1,54 @@
+#შექმენით ცვლადები სახელი გვარი ასაკი ჰობი ქვეყანა და ეს ყველა ცვლადი დაპრინტეთ
+
+name = "lasha"
+last_name = "abeslamidze"
+age = 13
+hoby = "play video games"
+country = "Georgia"
+
+print(name, last_name, age, hoby, country)
+
+
+#გააკეთე 10 მაგალით ყველა მათემატიკურ ოპერატორებზე + - * // **
+
+
+x = 40
+y = 30
+print(x + y)
+a = 90
+b = 60
+print(90 - 60)
+k = 40
+n = 60
+print(k * n)
+akm = 1280
+m4 = 4
+print(akm // m4)
+chiti = 15
+mtredi = 3
+print(chiti ** mtredi)
+scorpion = 100
+sab_ziro = 98
+print(scorpion * sab_ziro)
+jonny_caje = 60
+sonia_blade = 65
+print(jonny_caje + sonia_blade)
+shao_khan = 450
+kothal_khan = 60
+print(shao_khan // kothal_khan)
+mortal_kombat = 5000
+pubg = 3050
+print(5000 - 3050)
+minecraft = 50000
+teraria = 30000
+print(minecraft * teraria)
+
+
+
+
+#შექმენით ცვლადები გადაეცით ცვლადები გადაეცით მნიშვნელობები და შeმდგომ დაამატეთ ერთმანეთზე
+
+
+num1 = 57254
+num2 = 68349
+print(num1 + num2) 
