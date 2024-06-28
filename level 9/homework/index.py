f48c5457bff79ca97efe74cdcc94b99e683bf385
lashaabeslamidze <lashaabeslamidze10@gmail.com>
2024-06-28 09:44:19 +0300

    commit

---
+++ level 9/homework/index.py
@@ -0,0 +1,37 @@
+#1დაწერეთ პითონის პროგრამა რომელიც მოუწოდებს
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+
+print(num1 + num2)
+
+#2
+
+num1 = int(input("enter rectangle height: "))
+num2 = int(input("enter rectangle width: "))
+
+print(num1 * num2)
+
+
+#3
+
+
+num1 = int(input("enter number: "))
+
+print(num1 * 2)
+
+#4
+
+num1 = int(input("enter rectangle height: "))
+num2 = int(input("enter rectangle width: "))
+
+print(num1 * 2 + num2 * 2)
+
+#5
+num1 = int(input("enter number"))
+
+print(num1 ** 2)
+
+
+
+
