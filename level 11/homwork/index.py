afe3cefab3704f18298d0691fa8acfd92979ef2a
lashaabeslamidze <lashaabeslamidze10@gmail.com>
2024-06-30 09:11:06 +0300

    commit

---
+++ level 11/homwork/index.py
@@ -0,0 +1,70 @@
+#1) გააკეთეთ 30 მაგალითი შედარებით ოპერატორებზე.
+
+print(10 == 10)
+print(11 < 13)
+print(15 > 12)
+print(564 >= 235)
+print(473 != 125)
+print(362 <= 496)
+print(9574 == 9574)
+print(3745 < 4634)
+print(3736 > 2834)
+print(3546 < 9376)
+print(37456 <= 83475)
+print(47364 < 83646)
+print(26374 > 2534)
+print(3857784 <= 3857784)
+print(74856 > 8375)
+print(93857 != 63547)
+print(28465 < 37564)
+print(3665 == 3665)
+print(83746 > 36456)
+print(3757 == 3757)
+print(27465 > 6453)
+print(37746 < 384658)
+print(47564848 <= 3844353)
+print(3746466 < 736475885)
+print(374564 >= 35436)
+print(54673 == 54673)
+print(273546 != 13242)
+print(2274365 >= 1524357)
+
+#2) დაწერეთ პროგრამა, რომელიც იღებს ორი ადამიანის ასაკს და განსაზღვრავს ვინ არის უფროსი.
+
+#ნაბიჯები:
+
+#შესთავაზეთ მომხმარებელს შეიყვანოს პირველი პირის ასაკი.
+#სთხოვეთ მომხმარებელს შეიყვანოს მეორე პირის ასაკი.
+#შეადარეთ ორი ასაკი შედარების ოპერატორების გამოყენებით.
+#გამოიტანეთ შეტყობინება, რომელიც მიუთითებს იმაზე, თუ ვინ არის უფროსი ან იმავე ასაკის.
+
+
+savings1 = input('enter ur num1: ')
+savings2 = input('enter ur num2: ')
+
+updsavings1 = float(savings1)
+updsavings2 = float(savings2)
+
+balance = updsavings1 * 1,25
+balance = updsavings2 * 1,25
+
+balance1 = str(balance)
+balance2 = str(balance)
+
+message1 = 'amount in 1 year' + balance1
+message2 = 'amount in 1 year' + balance2
+
+print(message1)
+print(message2)
+
+num1 = int(input())
+num2 = int(input())
+
+if num1 > num2
+
+
+
+
+
+
+
