f8c2425ea38d23d89142eb71a4f38bd2f55ed54c
lashaabeslamidze <lashaabeslamidze10@gmail.com>
2024-07-02 02:24:44 +0300

    commit

--- level 11/homwork/index.py
+++ level 11/homwork/index.py
@@ -60,7 +60,7 @@ print(message2)
 num1 = int(input())
 num2 = int(input())
 
-if num1 > num2
+print(num1 > num2)
 
 
 
---
+++ level 15/classwork/index.py
@@ -0,0 +1,51 @@
+#გააკეთეთ 10 მაგალითი შედარებით ოპერატორებზე~
+
+print(36434 > 2314)
+print(26354 != 26354)
+print(35434 < 647454)
+print(217216 == 217216)
+print(2 >= 1)
+print(9 <= 9)
+print(7 <= 12)
+num1 = int(input())
+num2 = int(input())
+
+print(num1 > num2)
+
+num3 = int(input())
+num4 = int(input())
+
+print(num3 < num4)
+
+num5 = int(input())
+num6 = int(input())
+
+print(num5 > num6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
