d6a65a001eee3dbe1c70041b003f028bf9b966df
lashaabeslamidze <lashaabeslamidze10@gmail.com>
2024-07-29 05:50:47 +0300

    commit

---
+++ level 5/homework/index.py
@@ -0,0 +1,9 @@
+#1)შექმენით ცვლადები, სადაც თქვენს შესახებ შეინახავთ ინფორმაციას და ბოლოში დაბეჭდეთ ყველაფერი ერთ გრძელ წინადადებაში, ასევე დაუმატეთ თქვენს კოდს კომენტარები და კომენტრარებში ახსენით თითოეული ხაზი კოდი
+# me aq davprinte yvelaferi chems shesaxeb sheizleba gainteresebt ratom chavsvi brchxalebshi radgan brchxalebis gareshe kompiuteri erors gamoushvebda da qvemot teqst ar achvenebda
+print("gamarjoba me var lasha abeslamidze var 14 wlis dabadebis dge maqvs 14 ivlis vcxovrob batumshi vswavlob sauketeso akademia goashi zaan miyvars programireba da minda rom profesionaluri programisti gavxde")
+
+
+
+
+
+
