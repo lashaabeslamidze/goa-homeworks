513c9982eb7809be50c1f28ee57faeb87da9a74f
lashaabeslamidze <lashaabeslamidze10@gmail.com>
2024-08-03 00:44:03 +0300

    commit

---
+++ level 23/claswork/index.py
@@ -0,0 +1,36 @@
+
+number = int(input('enter number: '))
+guess_number = int(input('gues the number: '))
+
+while number != guess_number:
+    guess_again_number = int(input('guess again the number: '))
+    
+
+number = int(input())
+if number==25:
+    print("correct")
+
+else:
+  print("not corect")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
