458fec02ec9ed37a7f8204e58a17ce52c78932f5
lashaabeslamidze <lashaabeslamidze10@gmail.com>
2024-10-20 20:30:32 +0400

    commit

--- level 44/code wars/codewars.py
+++ level 44/code wars/codewars.py
@@ -1,4 +1,4 @@
-#Implement a function which convert the given boolean value into its string representation.
+#1Implement a function which convert the given boolean value into its string representation.
 
 # Note: Only valid inputs will be given.
 
@@ -6,27 +6,27 @@ def boolean_to_string(b):
     return str(b)
 
 #==============================
-# Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+#2 Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
 
 # [Make sure you type the exact thing I wrote or the program may not execute properly]
 
     return f'Hello, {name} how are you doing today?'
 
 #===============================================================================
-# This code does not execute properly. Try to figure out why.
+#3 This code does not execute properly. Try to figure out why.
 
 def multiply(a, b):
     return (a * b)
 
 #================================
-# Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+#4 Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
 
 def opposite(number):
     return -number
 
 #======================================
 
-# You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+#5 You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
 # If it is a square, return its area. If it is a rectangle, return its perimeter.
 
 # Example(Input1, Input2 --> Output):
@@ -34,14 +34,6 @@ def opposite(number):
 
 def area_or_perimeter(l , w):
     return l * w if l == w else (l + w) * 2
-
-#=================================================
-# Implement a function which convert the given boolean value into its string representation.
-
-# Note: Only valid inputs will be given.
-
-def boolean_to_string(b):
-    return str(b)
 
 
 
